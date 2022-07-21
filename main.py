45328f4d97146e48c61e4ca8c27a23a725f99ff1
Kalid Mulgado Sanchez <mandrive.00@gmail.com>
2022-07-20 21:09:51 -0500

    Just adding a file to create the repository

---
+++ main.py
@@ -0,0 +1,20 @@
+#import RPi.GPIO as GPIO
+import time
+
+"""
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(21, GPIO.OUT)
+
+try:
+    while True:
+        GPIO.output(21, GPIO.HIGH)
+        sleep(2)
+        GPIO.output(21, GPIO.LOW)
+        sleep(2)
+
+finally:
+    GPIO.cleanup()
+"""
+
